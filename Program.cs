7ee6796c36797a43532651697e52f2f2f5e0cb72
Nahidahmed <nahid.mansuri@gmail.com>
2016-10-05 07:39:48 +0530

    VS Code Project to practice c# programming

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArr = {1,2,3,4,5,6};
+            Console.WriteLine("Sum of Even Numbers in the array: "+ SumOfEvenNumbers(intArr));
+        }
+
+        static long SumOfEvenNumbers(int[] intArray){
+            return intArray.Where(i => i%2 == 0).Sum(i => (long)i);
+        }
+    }
+}
