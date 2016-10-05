5b2f8409800e5d2d56bdd44518829176fe033a73
Nahidahmed <nahid.mansuri@gmail.com>
2016-10-05 09:17:41 +0530

    StringReversal and Palindrome methods added

--- Program.cs
+++ Program.cs
@@ -9,10 +9,36 @@ namespace ConsoleApplication
         {
             int[] intArr = {1,2,3,4,5,6};
             Console.WriteLine("Sum of Even Numbers: "+ SumOfEvenNumbers(intArr));
+
+            //Out of 3 numbers find max value. Create an array of those number and call Array.Max()    
+            Console.WriteLine("Max Value: "+ intArr.Max());
+
+            //String Reversal
+            Console.WriteLine("Reverse of Nahid: "+ ReverseString("Nahid"));
+            //Palindrome
+            IsPalindromeString("icu");
         }
 
         static long SumOfEvenNumbers(int[] intArray){
             return intArray.Where(i => i%2 == 0).Sum(i => (long)i);
         }
+
+        static string ReverseString(string str){
+            char[] charArr = str.ToCharArray();
+            
+            return new string(charArr.Reverse().ToArray());
+        }
+
+        static void IsPalindromeString(string str)
+        {
+            char[] charsArr = str.ToCharArray();
+            string reverseStr = new string(charsArr.Reverse().ToArray());
+                
+            if(str == reverseStr)
+                Console.WriteLine("Its a Palindrome string: " +reverseStr);
+            else
+                Console.WriteLine("Its NOT a Palindrome string: " +reverseStr);
+        }
+        
     }
 }
